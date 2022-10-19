66fd2ffce43351a8376f88077634e147e8fd80f9
wismcorp <wismcorp@gmail.com>
2022-10-19 03:31:06 -0700

    Functions , nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 /**
  * print_alphabeth - prints the alpha in lowercase
  **/
-
 void print_alphabet(void)
 {
 	char la;
@@ -10,5 +9,5 @@ void print_alphabet(void)
 	for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
 
-	  	_putchar('\n');
+	  _putchar('\n');
 }
